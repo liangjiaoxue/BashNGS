cff0a65b520ac6a38e45c2b235cf15ecfc74c3c7
Liangjiao Xue <liangjiaoxue@users.noreply.github.com>
2016-10-07 10:37:28 -0400

    Update BashNGS.sh

--- BashNGS.sh
+++ BashNGS.sh
@@ -8,8 +8,7 @@
 cd fastq_dir
 ls -a | grep _L001_R1_001.fastq.gz | while read -r line ; do
    echo "Processing file $line"
-   y=${line%_L001_R1_001.fastq.gz}
-   sample=${y##*/}
+   sample=${line%_L001_R1_001.fastq.gz}   
    echo "Processing sample $sample"
    cat $sample*"_R1_001.fastq.gz" >$sample"_R1.fq.gz"
    cat $sample*"_R2_001.fastq.gz" >$sample"_R2.fq.gz"
